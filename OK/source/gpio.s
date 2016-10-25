a9b92e9e7ec74bb2d3e5c87b98d5439a7ac0867c
beazlr02 <ross.beazley@bbc.co.uk>
2016-10-25 14:10:31 +0100

    refactor gpio address to function

--- OK/source/gpio.s
+++ OK/source/gpio.s
@@ -2,3 +2,23 @@
 GetGpioAddress:
 ldr r0,=0x20200000
 mov pc,lr
+
+.globl SetGpioFunction
+SetGpioFunction:
+cmp r0,#53
+cmpls r1,#7
+movhi pc,lr
+
+mov r2,r0
+
+push {lr}
+bl GetGpioAddress
+
+# GPIO controller address has been returned into r0
+functionLoop$: # GPIO Controller Address + 4 × (GPIO Pin Number ÷ 10)
+cmp r2,#9
+subhi r2,#10
+addhi r0,#4
+bhi functionLoop$
+
+
