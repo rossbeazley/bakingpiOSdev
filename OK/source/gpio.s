dc0141efccd9dd29a34c9cda3e9956d1a4c9694d
beazlr02 <ross.beazley@bbc.co.uk>
2016-10-26 13:38:07 +0100

    more copy and paste from the tutorial, register aliases

--- OK/source/gpio.s
+++ OK/source/gpio.s
@@ -1,7 +1,9 @@
-.globl GetGpioAddress
+.globl GetGpioAddress /* this exports the symbol for the assembler */
 GetGpioAddress:
 ldr r0,=0x20200000
-mov pc,lr
+mov pc,lr /* return */
+
+
 
 .globl SetGpioFunction
 SetGpioFunction:
@@ -11,7 +13,7 @@ movhi pc,lr
 
 mov r2,r0
 
-push {lr}
+push {lr} /* going to do another branch so need to remember our return address */
 bl GetGpioAddress
 
 # GPIO controller address has been returned into r0
@@ -20,5 +22,31 @@ cmp r2,#9
 subhi r2,#10
 addhi r0,#4
 bhi functionLoop$
+
+add r2, r2,lsl #1
+lsl r1,r2
+str r1,[r0]
+pop {pc}
+
+
+.globl SetGpio
+SetGpio:
+pinNum .req r0  /* register alias */
+pinVal .req r1
+
+cmp pinNum,#53
+movhi pc,lr
+push {lr}
+mov r2,pinNum
+.unreq pinNum
+pinNum .req r2
+bl GetGpioAddress
+gpioAddr .req r0
+
+pinBank .req r3
+lsr pinBank,pinNum,#5
+lsl pinBank,#2
+add gpioAddr,pinBank
+.unreq pinBank
 
 
