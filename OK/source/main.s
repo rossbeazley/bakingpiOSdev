09e5413a6b37ae1805cdf525af11283b9464050b
beazlr02 <ross.beazley@bbc.co.uk>
2016-11-08 16:42:10 +0000

    Flash LED based on input sequence

--- OK/source/main.s
+++ OK/source/main.s
@@ -18,49 +18,50 @@ bl SetGpioFunction
 .unreq pinNum
 .unreq pinFunc
 
+ptrn .req r4
+ldr ptrn,=pattern
+ldr ptrn,[ptrn]
+seq .req r5
+mov seq,#0
+
 loop$: 
+
+
+/* creates a 000001 value,
+   left shifts by the seq number to get something like 001000
+   then and against the patern, we get a ZERO or if there is a 1 in the pattern at that point a NON-ZERO
+*/
+mov r1,#1
+lsl r1,seq
+and r1,ptrn
+
+
+push {ptrn, seq}
 
 /* turn pin 16 on */
 pinNum .req r0
 pinVal .req r1
 mov pinNum,#16
-mov pinVal,#0
+# mov pinVal,#0
 bl SetGpio
 .unreq pinNum
 .unreq pinVal
 
 # sleep a bit
-#mov r2,#0x0F0000
-#wait1$:
-#sub r2,#1
-#cmp r2,#0
-#bne wait1$
-# end sleep a bit
-mov r0, #4
+mov r0, #1
 bl WaitForSeconds
 
+pop {ptrn,seq}
 
-/* turn pin 16 off */
-pinNum .req r0
-pinVal .req r1
-mov pinNum,#16
-mov pinVal,#1
-bl SetGpio
-.unreq pinNum
-.unreq pinVal
-
-
-
-# sleep a bit
-mov r2,#0x0F0000
-wait2$:
-sub r2,#1
-cmp r2,#0
-bne wait2$
-# end sleep a bit
-
-mov r0, #2
-bl WaitForSeconds
-
+/* now increment sequence number modulo 32, ie 0-31 inclusive */
+add seq, seq, #1
+/* maybe we can use seq to do a ROR on the AND instead of LSL r1 by seq */
+and seq, seq, #63
 
 b loop$
+
+.section .data
+.align 2
+pattern:
+.int 0b11111111101010100010001000101010
+
