18d32edcc6aa562431ac63553518b66805424006
beazlr02 <ross.beazley@bbc.co.uk>
2016-11-14 13:01:11 +0000

    add transcribed postman code from tutorial

--- SCREEN/source/postman.s
+++ SCREEN/source/postman.s
@@ -8,25 +8,74 @@ mov pc,lr
    returns the message in r0  */
 .globl MailboxRead
 MailboxRead:
-/*
- mov r0 to r4 (mailbox)
- loop: 
- while Status bit 30 is not 0 loop
- load Read into r0
- if mailbox not matches r0 loop
- return Read (r0) 
-*/
+cmp r0,#15
+movhi pc,lr
+
+channel .req r1
+mov channel,r0
+push {lr}
+bl GetMailboxBase
+mailbox .req r0
+
+rightmail$:
+
+/**Wait until 30th bit is 0*/
+wait2$:
+status .req r2
+ldr status,[mailbox,#0x18]
+tst status,#0x40000000
+.unreq status
+bne wait2$
+
+mail .req r2
+ldr mail,[mailbox,#0]
+
+/** this will branch back to rightmail if this isnt a message for us*/
+nchan .req r3
+and inchan,mail,#0b1111
+teq inchan,channel
+.unreq inchan
+bne rightmail$
+.unreq mailbox
+.unreq channel
+
+/** return the contents of the message - ie the top 28 bits */
+and r0,mail,#0xfffffff0
+.unreq mail
+pop {pc}
 
 
 /* r0 contains a message to write (only the top 28 bits)
    r1 contains the mailbox channel */
 .globl MailboxWrite
-MailboxWrite:
-/*
-loop:
- while Status bit 32 is not 0 loop
- munge r0 and r1
- write to Write
-*/
+MailboxWrite: 
+tst r0,#0b1111
+movne pc,lr
+cmp r1,#15
+movhi pc,lr
 
+cannel .req r1
+value .req r2
+mov value,r0
+push {lr}
+bl GetMailboxBase
+mailbox .req r0
+
+/** wait for top bit of status to be 0 */
+wait1$:
+status .req r3
+ldr status,[mailbox,#0x18]
+tatus,#0x80000000
+.unreq status
+bne wait1$
+
+/** combine channel and message together and write to mailbox */
+add value,channel
+.unreq channel
+str value,[mailbox,#0x20]
+.unreq value
+.unreq mailbox
+
+/**return*/
+pop {pc}
 
